9842c181fabd3910f372acf28c4093038f6c8c24
trunglx5 <trungx1204@gmail.com>
2022-06-10 14:56:09 +0700

    App WPF

---
+++ WpfApp1/WpfApp1/Config.cs
@@ -0,0 +1,12 @@
+﻿namespace WpfApp1
+{
+    public static class Config
+    {
+        public static string FacePhotosPath = @"C:\Users\ttxid\source\repos\WpfApp1\WpfApp1\Faces";
+        public static string FaceListTextFile = @"C:\Users\ttxid\source\repos\WpfApp1\WpfApp1\FaceList.txt";
+        public static string HaarCascadePath = @"C:\Users\ttxid\source\repos\WpfApp1\WpfApp1\haarcascade_frontalface_default.xml";
+        public static int TimerResponseValue = 500;
+        public static string ImageFileExtension = ".bmp";
+        public static int ActiveCameraIndex = 0;//0: Default active camera device
+    }
+}
